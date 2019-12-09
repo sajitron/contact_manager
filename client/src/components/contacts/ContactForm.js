8202598b17df7a39989fc9ae09eda1aa6fc20ae3
sajicode <sajioloye@gmail.com>
2019-12-09 17:37:43 +0100

    modify update contact functionality

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,8 +1,28 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
 	const contactContext = useContext(ContactContext);
+
+	const { addContact, current, clearCurrent, updateContact } = contactContext;
+
+	useEffect(
+		() => {
+			if (current !== null) {
+				setContact(current);
+			} else {
+				setContact({
+					name: '',
+					email: '',
+					phone: '',
+					type: 'personal'
+				});
+			}
+		},
+		[ contactContext, current ]
+	);
+
+	//* we want useEffect to run if contactContext or current changes
 
 	const [ contact, setContact ] = useState({
 		name: '',
@@ -17,26 +37,49 @@ const ContactForm = () => {
 
 	const onSubmit = (e) => {
 		e.preventDefault();
-		contactContext.addContact(contact);
-		setContact({
-			name: '',
-			email: '',
-			phone: '',
-			type: 'personal'
-		});
+		if (current === null) {
+			addContact(contact);
+		} else {
+			updateContact(contact);
+		}
+		clearAll();
 	};
+
+	const clearAll = () => {
+		clearCurrent();
+	};
+
 	return (
 		<form onSubmit={onSubmit}>
-			<h2 className="text-primary">Add contact</h2>
+			<h2 className="text-primary">{current ? 'Edit Contact' : 'Add Contact'}</h2>
 			<input type="text" placeholder="name" name="name" value={name} onChange={onChange} />
 			<input type="text" placeholder="email" name="email" value={email} onChange={onChange} />
 			<input type="text" placeholder="phone" name="phone" value={phone} onChange={onChange} />
 			<h5>Contact Type</h5>
-			<input type="radio" name="type" value="personal" checked={type === 'personal'} onChange={onChange} /> Personal{' '}
-			<input type="radio" name="type" value="professional" checked={type === 'professional'} onChange={onChange} /> Professional
+			<input type="radio" name="type" value="personal" checked={type === 'personal'} onChange={onChange} />{' '}
+			Personal{' '}
+			<input
+				type="radio"
+				name="type"
+				value="professional"
+				checked={type === 'professional'}
+				onChange={onChange}
+			/>{' '}
+			Professional
 			<div>
-				<input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
+				<input
+					type="submit"
+					value={current ? 'Update Contact' : 'Add Contact'}
+					className="btn btn-primary btn-block"
+				/>
 			</div>
+			{current && (
+				<div>
+					<button className="btn btn-light btn-block" onClick={clearAll}>
+						Clear
+					</button>
+				</div>
+			)}
 		</form>
 	);
 };
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -66,6 +66,9 @@ const ContactState = (props) => {
 	};
 
 	//* Update contact
+	const updateContact = (contact) => {
+		dispatch({ type: UPDATE_CONTACT, payload: contact });
+	};
 
 	//* Filter contacts
 
@@ -79,7 +82,8 @@ const ContactState = (props) => {
 				addContact,
 				deleteContact,
 				setCurrent,
-				clearCurrent
+				clearCurrent,
+				updateContact
 			}}
 		>
 			{props.children}
