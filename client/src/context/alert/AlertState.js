5bb3ff3ffbe6fb5997ec1055e828c19221d6d789
sajicode <sajioloye@gmail.com>
2019-12-11 02:04:55 +0100

    move server contents to root

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid/v4';
+import uuid from 'uuid';
 import AlertContext from './alertContext';
 import alertReducer from './alertReducer';
 import { SET_ALERT, REMOVE_ALERT } from '../types';
--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 //* serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../client/build'));
+	app.use(express.static('client/build'));
 
-	app.get('*', (req, res) => res.sendFile(__dirname, '../', 'client', 'build', 'index.html'));
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
