2f67594516ab699a8f6baf73e27eaf5f5e5a8547
sajicode <sajioloye@gmail.com>
2019-12-11 01:36:38 +0100

    setup app for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const { config } = require('dotenv');
+const path = require('path');
 
 // read env variables before app initialization
 config();
@@ -17,6 +18,13 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//* serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('../client/build'));
+
+	app.get('*', (req, res) => res.sendFile(__dirname, '../', 'client', 'build', 'index.html'));
+}
 
 const PORT = process.env.PORT || 5000;
 
